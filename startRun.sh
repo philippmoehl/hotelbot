8efde942785e57f4ed322ba1b9c79b210d2f6f3e
philippmoehl <maxxphilipp@gmail.com>
2023-09-22 21:53:42 +0200

    updated version for support of non-linux machines

--- startRun.sh
+++ startRun.sh
@@ -1,6 +1,6 @@
 # build docker image from DockerfileServer
 # Rasa Server with spellchicking python package
-sudo docker build --tag rasa_custom:2.0 . -f DockerfileRasa
+sudo docker buildx build --platform linux/arm64 --tag rasa_custom:2.0 . -f DockerfileRasa
 
 # train and store model
 sudo docker run --user 1002 -it -v $(pwd):/app rasa_custom:2.0 train --domain domain.yml --data data --out models --config configs/config_spacy.yml --augmentation 20
